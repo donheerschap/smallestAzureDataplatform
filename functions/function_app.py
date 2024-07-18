37e161279bff7e19366b1aff9d4db56841e65714
Don Heerschap <don.heerschap@inspark.nl>
2024-07-18 08:13:40 +0200

    weatherapi

--- functions/function_app.py
+++ functions/function_app.py
@@ -2,7 +2,7 @@ import azure.functions as func
 import datetime
 import json
 import logging
-# import requests
+import requests
 
 app = func.FunctionApp()
 
@@ -32,12 +32,12 @@ def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
 def TestWeatherAPI(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    # data = requests.get(
-    #     'https://api.open-meteo.com/v1/forecast',
-    #     params={'latitude': '52.374', 'longitude': '4.8897', 'hourly': 'temperature_2m', 'past_days': '1', 'forecast_days': '1'}
-    #     ).json()
+    data = requests.get(
+        'https://api.open-meteo.com/v1/forecast',
+        params={'latitude': '52.374', 'longitude': '4.8897', 'hourly': 'temperature_2m', 'past_days': '1', 'forecast_days': '1'}
+        ).json()
     
     return func.HttpResponse(
-        'test',
+        data,
         status_code=200
     )
