9d4332a1d29a1346bdce192885cfa6e66506639c
Don Heerschap <don.heerschap@inspark.nl>
2024-07-18 20:07:15 +0200

    possible fix

--- functions/function_app.py
+++ functions/function_app.py
@@ -23,12 +23,12 @@ def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.2",
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.3",
              status_code=200
         )
     
 @app.route(route="TestWeatherAPI", auth_level=func.AuthLevel.ANONYMOUS)
-@app.blob_output(arg_name='datalake', path='output/test.json', connection='DATALAKE__serviceUri')
+@app.blob_output(arg_name='datalake', path='raw/test.json', connection='DATALAKE')
 def TestWeatherAPI(req: func.HttpRequest, datalake: func.Out[str]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
