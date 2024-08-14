7d11696f5a09fef2b68208ec5cb55bdc6aa56b23
Don Heerschap <don.heerschap@inspark.nl>
2024-08-14 20:36:44 +0200

    error handling

--- functions/function_app.py
+++ functions/function_app.py
@@ -27,15 +27,31 @@ def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
              status_code=200
         )
     
+def generate_blob_path():
+    current_datetime = datetime.datetime.now()
+    date_str = current_datetime.strftime('%Y-%m-%d')
+    unique_id = str(uuid.uuid4())
+    blob_path = f'raw/{date_str}/{unique_id}.json'
+    return blob_path
+
+_blob_path = generate_blob_path()
+    
 @app.route(route="TestWeatherAPI", auth_level=func.AuthLevel.ANONYMOUS)
-@app.blob_output(arg_name='datalake', path='raw/test.json', connection='DATALAKE')
+@app.blob_output(arg_name='datalake', path=_blob_path, connection='DATALAKE')
 def TestWeatherAPI(req: func.HttpRequest, datalake: func.Out[str]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    data = requests.get(
-        'https://api.open-meteo.com/v1/forecast',
-        params={'latitude': '52.374', 'longitude': '4.8897', 'hourly': 'temperature_2m', 'past_days': '1', 'forecast_days': '1'}
-        ).json()
+    try:
+        data = requests.get(
+            'https://api.open-meteo.com/v1/forecast',
+            params={'latitude': '52.374', 'longitude': '4.8897', 'hourly': 'temperature_2m', 'past_days': '1', 'forecast_days': '1'}
+            ).json()
+    except Exception as e:
+        logging.error(e)
+        return func.HttpResponse(
+            f"Failed to get data from weather API with error {e}",
+            status_code=500
+        )
     
     try:
         datalake.set(json.dumps(data))
