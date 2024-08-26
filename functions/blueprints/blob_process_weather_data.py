014170bcfd594f294b8584228b281fe69c339260
Don Heerschap <don.heerschap@inspark.nl>
2024-08-26 21:56:14 +0200

    logging

--- functions/blueprints/blob_process_weather_data.py
+++ functions/blueprints/blob_process_weather_data.py
@@ -13,29 +13,21 @@ bp = func.Blueprint()
     arg_name="sourceblob", path="bronze/weatherdata/{name}.json", connection="DATALAKE"
 )
 def process_blob_weather_data(sourceblob: blob.BlobClient):
+    logging.info('Starting to process blob')
     logging.info(
-        f"Python blob trigger function processed blob 2 \n"
+        f"Python blob trigger function processed blob \n"
         f"Properties: {sourceblob.get_blob_properties()}\n"
         f"Blob content head: {sourceblob.download_blob().read(size=1)}"
     )
-    account_url = os.environ['DATALAKE__blobServiceUri']
-    default_credential = DefaultAzureCredential()
-    dest_blob = BlobClient(account_url, credential=default_credential, container_name='silver', blob_name=f'weatherdata/{sourceblob.name}')
-    dest_blob.upload_blob(sourceblob.read())
-    logging.info(f"Blob {sourceblob.name} copied to silver container")
-
-@bp.function_name(name="sourceblob2")
-@bp.blob_trigger(
-    arg_name="sourceblob2", path="bronze/weatherdata/{name}", connection="DATALAKE"
-)
-def process_blob_weather_data2(sourceblob2: blob.BlobClient):
-    logging.info(
-        f"Python blob trigger function processed blob 2 \n"
-        f"Properties: {sourceblob2.get_blob_properties()}\n"
-        f"Blob content head: {sourceblob2.download_blob().read(size=1)}"
-    )
-    account_url = os.environ['DATALAKE__blobServiceUri']
-    default_credential = DefaultAzureCredential()
-    dest_blob = BlobClient(account_url, credential=default_credential, container_name='silver', blob_name=f'weatherdata2/{sourceblob2.name}')
-    dest_blob.upload_blob(sourceblob2.read())
-    logging.info(f"Blob {sourceblob2.name} copied to silver container")
+    try:
+        account_url = os.environ['DATALAKE__blobServiceUri']
+        logging.info(f"account_url: {account_url}")
+    except Exception as e:
+        logging.error(e)
+    try:
+        default_credential = DefaultAzureCredential()
+        dest_blob = BlobClient(account_url, credential=default_credential, container_name='silver', blob_name=f'weatherdata/{sourceblob.name}')
+        dest_blob.upload_blob(sourceblob.read())
+        logging.info(f"Blob {sourceblob.name} copied to silver container")
+    except Exception as e:
+        logging.error(e)
