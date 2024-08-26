7a7d20de9090dcfc678224dd6e987a791b880d0e
Don Heerschap <don.heerschap@inspark.nl>
2024-08-26 15:42:27 +0200

    register processing function

--- functions/function_app.py
+++ functions/function_app.py
@@ -3,9 +3,11 @@ import azure.functions as func
 from blueprints.cron_extract_weather_data import bp as bp_cron_extract_weather_data
 from blueprints.http_hello_world import bp as bp_http_hello_world
 from blueprints.http_extract_weather_data import bp as bp_http_extract_weather_data
+from blueprints.blob_process_weather_data import bp as bp_blob_process_weather_data
 
 app = func.FunctionApp()
 
 app.register_functions(bp_http_hello_world)
 app.register_functions(bp_cron_extract_weather_data)
 app.register_functions(bp_http_extract_weather_data)
+app.register_functions(bp_blob_process_weather_data)
