0f4b57a787e5e71e4eabc55b43452533076cd9a9
Don Heerschap <don.heerschap@inspark.nl>
2024-08-26 18:40:15 +0200

    removed underscore bcs error

--- functions/blueprints/blob_process_weather_data.py
+++ functions/blueprints/blob_process_weather_data.py
@@ -8,18 +8,18 @@ import azurefunctions.extensions.bindings.blob as blob
 
 bp = func.Blueprint()
 
-@bp.function_name(name="source_blob")
+@bp.function_name(name="sourceblob")
 @bp.blob_trigger(
-    arg_name="source_blob", path="bronze/weatherdata", connection="DATALAKE"
+    arg_name="sourceblob", path="bronze/weatherdata", connection="DATALAKE"
 )
-def process_blob_weather_data(source_blob: blob.BlobClient):
+def process_blob_weather_data(sourceblob: blob.BlobClient):
     logging.info(
         f"Python blob trigger function processed blob \n"
-        f"Properties: {source_blob.get_blob_properties()}\n"
-        f"Blob content head: {source_blob.download_blob().read(size=1)}"
+        f"Properties: {sourceblob.get_blob_properties()}\n"
+        f"Blob content head: {sourceblob.download_blob().read(size=1)}"
     )
     account_url = os.environ['DATALAKE__blobServiceUri']
     default_credential = DefaultAzureCredential()
-    dest_blob = BlobClient(account_url, credential=default_credential, container_name='silver', blob_name=f'weatherdata/{source_blob.name}')
-    dest_blob.upload_blob(source_blob.read())
-    logging.info(f"Blob {source_blob.name} copied to silver container")
+    dest_blob = BlobClient(account_url, credential=default_credential, container_name='silver', blob_name=f'weatherdata/{sourceblob.name}')
+    dest_blob.upload_blob(sourceblob.read())
+    logging.info(f"Blob {sourceblob.name} copied to silver container")
