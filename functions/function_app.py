0f0c03515c6f64e93c236a4ddd0882edca23bf80
Don Heerschap <don.heerschap@inspark.nl>
2025-05-06 11:46:13 +0200

    Update function_app.py

--- functions/function_app.py
+++ functions/function_app.py
@@ -7,7 +7,7 @@ from blueprints.blob_process_weather_data import bp as bp_blob_process_weather_d
 
 app = func.FunctionApp()
 
-# app.register_functions(bp_http_hello_world)
+app.register_functions(bp_http_hello_world)
 app.register_functions(bp_cron_extract_weather_data)
 app.register_functions(bp_http_extract_weather_data)
 app.register_functions(bp_blob_process_weather_data)
